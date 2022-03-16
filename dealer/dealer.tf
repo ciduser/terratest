ea5bff2ae6129c18bc2e5d106c03243787131c56
cid esquivel <101591165+ciduser@users.noreply.github.com>
2022-03-16 14:03:13 -0600

    Create dealer.tf

---
+++ dealer/dealer.tf
@@ -0,0 +1,28 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_KEY_ID" {}
+
+provider "aws" {
+    region = var.region
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_KEY_ID
+}
+
+/*------------bucket------------*/
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = var.bucket
+  acl = "public-read"
+  website {
+      index_document = "index.html"
+  }
+}
+
+/*------------content------------*/
+
+resource "aws_s3_bucket_object" "object1" {
+  bucket = aws_s3_bucket.bucket1.bucket
+  key    = "index.html"
+  acl = "public-read"
+  content = var.content
+  content_type = "text/html"
+}

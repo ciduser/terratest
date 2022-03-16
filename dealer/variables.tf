26267818a1aaeb6eaa9d722b7d4d0120e719b898
cid esquivel <101591165+ciduser@users.noreply.github.com>
2022-03-16 14:02:06 -0600

    Create variables.tf

---
+++ dealer/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket" { 
+    type = string
+    default = "buckets3jav"
+}
+
+variable "content" { 
+    type = string
+    default = "Class Terraform ROCKS!!!"
+}
+
+variable "region" {
+    type = string
+    default = "us-east-2"
+}

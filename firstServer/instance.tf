b0b9bf63f19bf5bc1a7fc4a12002e3607c55bc18
cid esquivel <101591165+ciduser@users.noreply.github.com>
2022-03-16 12:05:36 -0600

    Create instance.tf

---
+++ firstServer/instance.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+# Resource configuration
+resource "aws_instance" "hello-instance" {
+  ami = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "hello-instance"
+  }
+}
